397c23eddf9e18802392d91470de729ab212f9e4
Shamanth K Murthy <shamanthk2404@gmail.com>
2023-12-02 18:38:35 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
 dotenv.config();
 
 mongoose.connect("mongodb+srv://shamanthk2404:Shamanth24@realestate.qpixd16.mongodb.net/?retryWrites=true&w=majority").then(()=>{
@@ -11,3 +12,8 @@ mongoose.connect("mongodb+srv://shamanthk2404:Shamanth24@realestate.qpixd16.mong
 
 const app = express();
 
+app.listen(3000,()=>{
+    console.log('server running in 3000');
+});
+
+app.get("/api/user", userRouter);
