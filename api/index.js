8035796b984f1b5e51c466cfbbc219ddb14ceba8
Shamanth K Murthy <shamanthk2404@gmail.com>
2023-12-02 18:17:32 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -3,10 +3,11 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(()=>{
+mongoose.connect("mongodb+srv://shamanthk2404:Shamanth24@realestate.qpixd16.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log('connected to MongoDB');
 }).catch((err)=>{
     console.log(err);
 });
 
 const app = express();
+
