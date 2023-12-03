5155bf8f1543dc7b7a5de3f8002f5c01d7cdd3a4
Shamanth K Murthy <shamanthk2404@gmail.com>
2023-12-03 19:20:03 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect("mongodb+srv://shamanthk2404:Shamanth24@realestate.qpixd16.mongodb.net/?retryWrites=true&w=majority").then(()=>{
@@ -16,4 +17,5 @@ app.listen(3000,()=>{
     console.log('server running in 3000');
 });
 
-app.get("/api/user", userRouter);
+app.use("/api/user", userRouter);
+app.use('/api/user', authRouter);
