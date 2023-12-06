7b6b859ec34c6cd74d2ad6dd388126864ba5dd5f
Shamanth K Murthy <shamanthk2404@gmail.com>
2023-12-06 07:53:32 +0530

    sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcrypt.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req,res,next)=>{
     const {username,email,password} = req.body;
@@ -12,3 +13,18 @@ export const signup = async (req,res,next)=>{
         next(error);
     }
 };
+
+export const signIn = async (req,res,next) =>{
+    const {email,password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+    if(!validUser) return next(errorHandler(404,'User not found'));
+    const validPassword = bcrypt.compareSync(password,validUser.password);
+    if(!validPassword) return next(errorHandler(401,'Wrong Credentials'));
+    const token = jwt.sign({id:validUser._id},'shamanth');
+    const {password:pass, ...rest} = validUser._doc;
+    res.cookie('accessToken',token,{httpOnly:true}).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
